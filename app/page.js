af2d46cab13e82fd1a6484e6f6a6ee3618510b6e
Pamyc <163673087+Pamyc@users.noreply.github.com>
2025-11-06 08:56:47 +0300

    Delete app directory

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <main>
-      <h1>Привет, Timeweb Cloud! 👋</h1>
-      <p>Это Next.js (App Router) + Dockerfile. Страница: <strong>Hello World</strong>.</p>
-    </main>
-  );
-}
